447b697b5e482c7e93736ba4ba82000de97a329f
Ben Leung <benjamin.leung80@gmail.com>
2018-11-07 15:19:10 +0800

    Add files via upload

--- 6.3/main.py
+++ 6.3/main.py
@@ -12,57 +12,52 @@ strip = neopixel.NeoPixel(pixpin, numpix, brightness=0.1, auto_write=False)
 
 
 def lookupColor(color,i):
-    if color == 0:
-        strip[i] = (255,0,0)
+    if color == 10:
+        strip[i] = (0,0,0)
     # orange
-    elif color == 1:
+    elif color == 11:
         strip[i] = (255,179,0)
     # yellow
-    elif color == 2:
+    elif color == 12:
         strip[i] = (255,255,0)
     # green
-    elif color == 3:
+    elif color == 13:
         strip[i] = (0,255,0)
     # blue
-    elif color == 4:
+    elif color == 14:
         strip[i] = (0,0,255)
     # violet
-    elif color == 5:
+    elif color == 15:
         strip[i] = (62,0,105)
-    # pink
-    elif color == 7:
-        strip[i] = (247,50,255)
-    # black
-    elif color == 9:
-        strip[i] = (0,0,0)
+    # red
+    elif color == 17:
+        strip[i] = (255,0,0)
 
 
 
-def showMessage(nextMessage,duration,myMessage,myMaxLength,myCountToMax):
+
+def showMessage(nextMessage,duration,myMessage,myMaxLength):
     # start - show Message
     i = 0
-
     for r in range(0,8):
-        for c in range(myCountToMax,myCountToMax+8):
-            temp = c % globalvariables.maxLengthOfAdvertisement1
+        for c in range(globalvariables.countToMaxLength,globalvariables.countToMaxLength+8):
+            temp = c % myMaxLength
             color = myMessage[r][temp]
             lookupColor(color,i)
             i += 1
 
     for r in range(0,8):
-        for c in range(myCountToMax+8,myCountToMax+16):
-            temp = c % 
+        for c in range(globalvariables.countToMaxLength+8,globalvariables.countToMaxLength+16):
+            temp = c % myMaxLength
             color = myMessage[r][temp]
             lookupColor(color,i)
             i += 1
-
 
     strip.write()
-    time.sleep(duration)
 
-    myCountToMax += 1
-    if myCountToMax > myMaxLength:
-        myCountToMax = 0
+    globalvariables.countToMaxLength += 1
+    if globalvariables.countToMaxLength > duration or globalvariables.countToMaxLength > myMaxLength:
+        globalvariables.countToMaxLength = 0
         globalvariables.messageID = nextMessage
 
 
@@ -71,30 +66,23 @@ def showMessage(nextMessage,duration,myMessage,myMaxLength,myCountToMax):
 
 while True:
     if globalvariables.messageID == 1:
-        # shortcut for message 1
-        strip.write()
-        # showMessage(nextMessage,messageDuration,message,maxLengthOfAdvertisement,countToMax)
-        showMessage(2,1,globalvariables.message1,globalvariables.maxLengthOfAdvertisement1,globalvariables.countToMaxLength1)
-
+        # Call function - showMessage(nextMessageID, messageDuration, message, maxLengthOfAdvertisement)
+        showMessage(2,12,globalvariables.message1,globalvariables.maxLengthOfAdvertisement1)
 
 
     elif globalvariables.messageID == 2:
-        # shortcut for message 2
-        strip.write()
-        showMessage(3,2,globalvariables.message2,globalvariables.maxLengthOfAdvertisement2,globalvariables.countToMaxLength2)
-
+        # Call function - showMessage(nextMessageID, messageDuration, message, maxLengthOfAdvertisement)
+        showMessage(3,10,globalvariables.message2,globalvariables.maxLengthOfAdvertisement2)
 
 
     elif globalvariables.messageID == 3:
-        # shortcut for message 3
-        strip.write()
-        showMessage(4,1,globalvariables.message3,globalvariables.maxLengthOfAdvertisement3,globalvariables.countToMaxLength3)
+        # Call function - showMessage(nextMessageID, messageDuration, message, maxLengthOfAdvertisement)
+        showMessage(4,5,globalvariables.message3,globalvariables.maxLengthOfAdvertisement3)
 
 
     elif globalvariables.messageID == 4:
-        # shortcut for message 4
-        strip.write()
-        showMessage(1,2,globalvariables.message4,globalvariables.maxLengthOfAdvertisement4,globalvariables.countToMaxLength4)
+        # Call function - showMessage(nextMessageID, messageDuration, message, maxLengthOfAdvertisement)
+        showMessage(1,5,globalvariables.message4,globalvariables.maxLengthOfAdvertisement4)
 
 
 
