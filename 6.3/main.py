eae652c5ec1ca992fa5f6c56e52a8ef30d1f85d2
Ben Leung <benjamin.leung80@gmail.com>
2018-11-08 08:35:07 +0800

    Add files via upload

--- 6.3/main.py
+++ 6.3/main.py
@@ -34,21 +34,28 @@ def lookupColor(color,i):
         strip[i] = (255,0,0)
 
 
+# ----------------------------------------------------------------------------------------------------
+# START DO NOT MODIFY THE CODE BETWEEN THE ---
+# ----------------------------------------------------------------------------------------------------
 
 
-def showMessage(nextMessage,duration,myMessage,myMaxLength):
+def showMessageL(nextMessage,duration,myMessage,startingPoint):
     # start - show Message
+    if globalvariables.myStart == 0:
+        globalvariables.myStart = 1
+        globalvariables.countToMaxLength = startingPoint
+
     i = 0
     for r in range(0,8):
         for c in range(globalvariables.countToMaxLength,globalvariables.countToMaxLength+8):
-            temp = c % myMaxLength
+            temp = c % len(myMessage[0])
             color = myMessage[r][temp]
             lookupColor(color,i)
             i += 1
 
     for r in range(0,8):
         for c in range(globalvariables.countToMaxLength+8,globalvariables.countToMaxLength+16):
-            temp = c % myMaxLength
+            temp = c % len(myMessage[0])
             color = myMessage[r][temp]
             lookupColor(color,i)
             i += 1
@@ -56,34 +63,142 @@ def showMessage(nextMessage,duration,myMessage,myMaxLength):
     strip.write()
 
     globalvariables.countToMaxLength += 1
-    if globalvariables.countToMaxLength > duration or globalvariables.countToMaxLength > myMaxLength:
+    globalvariables.durationCounter += 1
+    if globalvariables.countToMaxLength > duration:
         globalvariables.countToMaxLength = 0
+        globalvariables.myStart = 0
+        globalvariables.durationCounter = 0
+        globalvariables.messageID = nextMessage
+
+
+
+def showMessageR(nextMessage,duration,myMessage,startingPoint):
+    # start - show Message
+    if globalvariables.myStart == 0:
+        globalvariables.myStart = 1
+        globalvariables.countToMaxLength = startingPoint
+
+        
+
+    i = 0
+    for r in range(0,8):
+        for c in range(globalvariables.countToMaxLength,globalvariables.countToMaxLength+8):
+            temp = c % len(myMessage[0])
+            color = myMessage[r][temp]
+            lookupColor(color,i)
+            i += 1
+
+    for r in range(0,8):
+        for c in range(globalvariables.countToMaxLength+8,globalvariables.countToMaxLength+16):
+            temp = c % len(myMessage[0])
+            color = myMessage[r][temp]
+            lookupColor(color,i)
+            i += 1
+
+    strip.write()
+
+    globalvariables.countToMaxLength -= 1
+    globalvariables.durationCounter += 1
+    if globalvariables.durationCounter > duration:
+        globalvariables.countToMaxLength = 0
+        globalvariables.myStart = 0
+        globalvariables.durationCounter = 0
         globalvariables.messageID = nextMessage
 
 
 
 
+def showMessageU(nextMessage,duration,myMessage,startingPoint):
+    # start - show Message
+    if globalvariables.myStart == 0:
+        globalvariables.myStart = 1
+        globalvariables.countToMaxLength = startingPoint
+
+        
+
+    i = 0
+    for r in range(globalvariables.countToMaxLength,globalvariables.countToMaxLength+8):
+        for c in range(0,8):
+            temp = r % len(myMessage)
+            color = myMessage[temp][c]
+            lookupColor(color,i)
+            i += 1
+
+    for r in range(globalvariables.countToMaxLength,globalvariables.countToMaxLength+8):
+        for c in range(8,16):
+            temp = r % len(myMessage)
+            color = myMessage[temp][c]
+            lookupColor(color,i)
+            i += 1
+
+    strip.write()
+
+    globalvariables.countToMaxLength -= 1
+    globalvariables.durationCounter += 1
+    if globalvariables.durationCounter > duration:
+        globalvariables.countToMaxLength = 0
+        globalvariables.myStart = 0
+        globalvariables.durationCounter = 0
+        globalvariables.messageID = nextMessage
+
+
+
+def showMessageD(nextMessage,duration,myMessage,startingPoint):
+    # start - show Message
+    if globalvariables.myStart == 0:
+        globalvariables.myStart = 1
+        globalvariables.countToMaxLength = startingPoint
+
+        
+
+    i = 0
+    for r in range(globalvariables.countToMaxLength,globalvariables.countToMaxLength+8):
+        for c in range(0,8):
+            temp = r % len(myMessage)
+            color = myMessage[temp][c]
+            lookupColor(color,i)
+            i += 1
+
+    for r in range(globalvariables.countToMaxLength,globalvariables.countToMaxLength+8):
+        for c in range(8,16):
+            temp = r % len(myMessage)
+            color = myMessage[temp][c]
+            lookupColor(color,i)
+            i += 1
+
+    strip.write()
+
+    globalvariables.countToMaxLength += 1
+    globalvariables.durationCounter += 1
+    if globalvariables.durationCounter > duration:
+        globalvariables.countToMaxLength = 0
+        globalvariables.myStart = 0
+        globalvariables.durationCounter = 0
+        globalvariables.messageID = nextMessage
+
+
+# ----------------------------------------------------------------------------------------------------
+# END DO NOT MODIFY THE CODE BETWEEN THE ---
+# ----------------------------------------------------------------------------------------------------
+
+
 
 while True:
     if globalvariables.messageID == 1:
-        # Call function - showMessage(nextMessageID, messageDuration, message, maxLengthOfAdvertisement)
-        showMessage(2,12,globalvariables.message1,globalvariables.maxLengthOfAdvertisement1)
+        # Call function - showMessage(nextMessageID, messageDuration, message, start)
+        showMessageL(2,13,globalvariables.message1,0)
 
 
     elif globalvariables.messageID == 2:
-        # Call function - showMessage(nextMessageID, messageDuration, message, maxLengthOfAdvertisement)
-        showMessage(3,10,globalvariables.message2,globalvariables.maxLengthOfAdvertisement2)
+        # Call function - showMessage(nextMessageID, messageDuration, message, start)
+        showMessageR(3,10,globalvariables.message2,15)
 
 
     elif globalvariables.messageID == 3:
-        # Call function - showMessage(nextMessageID, messageDuration, message, maxLengthOfAdvertisement)
-        showMessage(4,5,globalvariables.message3,globalvariables.maxLengthOfAdvertisement3)
+        # Call function - showMessage(nextMessageID, messageDuration, message, start)
+        showMessageU(4,3,globalvariables.message3,10)
 
 
     elif globalvariables.messageID == 4:
-        # Call function - showMessage(nextMessageID, messageDuration, message, maxLengthOfAdvertisement)
-        showMessage(1,5,globalvariables.message4,globalvariables.maxLengthOfAdvertisement4)
-
-
-
-
+        # Call function - showMessage(nextMessageID, messageDuration, message, start)
+        showMessageD(1,15,globalvariables.message4,4)
